67e2ec32e8fdbd1f6e8ef120ba886e3194a03f91
pradeep <pradeepdevops93@gmail.com>
2025-05-02 14:08:57 -0500

    modified to store output in diff file

--- backend.sh
+++ backend.sh
@@ -1,18 +1,18 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-useradd expense
-cp backend.service /etc/systemd/system/backend.service
-rm -rf /app
+dnf module disable nodejs -y >/tmp/expense.log
+dnf module enable nodejs:20 -y >/tmp/expense.log
+dnf install nodejs -y >/tmp/expense.log
+useradd expense >/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service >/tmp/expense.log
+rm -rf /app >/tmp/expense.log
 
-mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
-unzip /tmp/backend.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
-dnf install mysql -y
-mysql -h 3.238.4.99 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mkdir /app >/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip >/tmp/expense.log
+cd /app >/tmp/expense.log
+unzip /tmp/backend.zip >/tmp/expense.log
+cd /app >/tmp/expense.log
+npm install >/tmp/expense.log
+systemctl daemon-reload >/tmp/expense.log
+systemctl enable backend >/tmp/expense.log
+systemctl start backend >/tmp/expense.log
+dnf install mysql -y >/tmp/expense.log
+mysql -h 3.238.4.99 -uroot -pExpenseApp@1 < /app/schema/backend.sql >/tmp/expense.log
