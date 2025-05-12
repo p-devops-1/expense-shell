1b80ea860c2ecf153cc14e42ba2bf82c1e46c49e
pradeep <pradeepdevops93@gmail.com>
2025-05-12 11:48:24 -0500

    updated IP address

--- backend.sh
+++ backend.sh
@@ -1,57 +1,62 @@
 mysql_root_password=$1
 
-echo Disable Default NodeJS Version Module
+Print_Task_Heading (){
+  echo $1
+  echo "################ $1 ###########" &>>/tmp/expense.log
+}
+
+Print_Task_Heading "Disable Default NodeJS Version Module"
 dnf module disable nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo Enable NodeJS Module for v20
+Print_Task_Heading "Enable NodeJS Module for v20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
 echo $?
 
-echo Install NodeJS
+Print_Task_Heading "Install NodeJS"
 dnf install nodejs -y &>>/tmp/expense.log
 echo $?
 
-echo Adding Application User
+Print_Task_Heading "Adding Application User"
 useradd expense &>>/tmp/expense.log
 echo $?
 
-echo Copy backend service file
+Print_Task_Heading "Copy backend service file"
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 echo $?
 
-echo clean the old content
+Print_Task_Heading "clean the old content"
 rm -rf /app &>>/tmp/expense.log
 echo $?
 
-echo create App directory
+Print_Task_Heading "create App directory"
 mkdir /app &>>/tmp/expense.log
 echo $?
 
-echo Download App content
+Print_Task_Heading "Download App content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
 
-echo Extract App content
+Print_Task_Heading "Extract App content"
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
 echo $?
 
-echo Download NodeJS Dependencies
+Print_Task_Heading "Download NodeJS Dependencies"
 cd /app &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
 echo $?
 
-echo start Backend Service
+Print_Task_Heading "start Backend Service"
 systemctl daemon-reload &>>/tmp/expense.log
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
 echo $?
 
-echo Install MySQL client
+Print_Task_Heading "Install MySQL client"
 dnf install mysql -y &>>/tmp/expense.log
 echo $?
 
-echo Load Schema
+Print_Task_Heading "Load Schema"
 mysql -h 172.31.90.147 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>/tmp/expense.log
 echo $?
